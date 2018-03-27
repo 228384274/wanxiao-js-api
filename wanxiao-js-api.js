7766acd2c81bb42047b00ef83fbb5be8d54fbc6c
liuchang <liuchang8877@gmail.com>
2018-03-27 14:46:15 +0800

    Update wanxiao-js-api.js
    更新扫一扫方法

--- wanxiao-js-api.js
+++ wanxiao-js-api.js
@@ -404,9 +404,9 @@ Wanxiao.prototype.scanBarcode = function (callback,jsonStr) {
     var params = JSON.stringify(jsonStr);
 	
     if (!isIphone()) {
-        Wanxiao.prototype._scanBarcodeCallback = callback;
-        window.wanxiao_scanBarcode.executeBindMethod("scanBarcode",
-                                                     params);
+
+         callback(window.wanxiao_scanBarcode.executeBindMethod("scanBarcode",
+                                                     params));
     } else {
         
         var postJsonObject = {
